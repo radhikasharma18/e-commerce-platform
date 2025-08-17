983bf59a17da55d1be2868164253c165f19ee04d
radhikasharma18 <sharmaradhika1803@gmail.com>
2025-08-17 21:53:28 +0530

    half set done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,71 @@ import cat6 from '../assets/cat6.png.png';
 import cat7 from '../assets/cat7.png.png';
 import cat8 from '../assets/cat8.png.png';
 import { IoIosArrowRoundForward } from "react-icons/io";
+import product1 from '../assets/prod1.png'
+import product2 from '../assets/prod2.png'
+import product3 from '../assets/prod3.png'
+import product4 from '../assets/prod4.png'
+import product5 from '../assets/prod5.png'
+import Vectore from '../assets/Vector.png'
+import Productcard from './Productcard';
+import { IoMdHeart } from "react-icons/io";
+
+
+
+
+
 const Home = () => {
+  const productlist=[
+    { Image:product1,
+      name:"Royal Canin Cat Dry Food Exigent 2kg",
+      picture:Vectore,
+      salesprice:"$395",
+      label:"$100",
+      price:"$500",
+      likes:"(5)",
+      ship:"free shiping",
+      stock:"In stock"
+  },
+     {Image:product2,
+      name:"Brit Premium by Nature Cat Can – Chicken (200g)",
+      picture:Vectore,
+      salesprice:"$590",
+      label:"$3.00",
+      price:"$610",
+      likes:"(152)",
+      ship:"Pre Order",
+      stock:"Out of Stock"
+  },
+   {  Image:product3,
+      name:"Spectrum Starter32 Kitten Food – Chicken Formula (400g)",
+      picture:Vectore,
+      salesprice:"$1729",
+      label:"$3.00",
+      price:"$2110",
+      likes:"(152)",
+      ship:"Pre Order",
+      stock:"Out of Stock"
+  }, 
+  {   Image:product4,
+      name:"Himalaya Erina EP Tick and Flea Control Shampoo 200 ml",
+      picture:Vectore,
+      salesprice:"$979",
+      label:"$3.00",
+      price:"$1259",
+      likes:"(152)",
+      ship:"Pre Order",
+      stock:"In stock"
+  },  
+  {   Image:product5,
+      name:"Bonnie Beef Cocktail (500g)",
+      picture:Vectore,
+      salesprice:"$9",
+      label:"$3.00",
+      price:"$12",
+      likes:"(152)",
+      ship:"Free Shipping",
+      stock:<><IoMdHeart /> Out of Stock</>
+  },]
   return (
     <div className="p-8 ">
       <div className="md:h-[100vh] h-[140vh] w-[100%]  flex flex-col md:flex-row">
@@ -115,7 +179,27 @@ const Home = () => {
     <div className='text-xl font-serif text-[#362526] font-bold'>Hand-Picked Food</div>
     <div className='flex items-center gap-2 group hover:text-lg hover:underline '>View All <span className='text-2xl group-hover:text-4xl hover-underline'><IoIosArrowRoundForward /></span></div>
   </div>
-  <div></div>
+  <div className="relative  px-4 mx-4 flex flex-row lg:justify-start justify-center">
+        <div className='flex gap-8 md:gap-2 mt-16 flex-col md:flex-row px-8'>
+          {productlist.map((Product,index)=> (
+            <Productcard 
+  key={index}
+  sales={Product.label}
+  name={Product.name}
+  price={Product.price}
+  salesprice={Product.salesprice}
+  picture={Product.Image}
+  vector={Product.picture}
+  instock={Product.instock}
+  ship={Product.ship}
+  likes={Product.likes}
+  stock={Product.stock}
+/>
+          ))}
+          
+        </div>
+
+      </div>
 </div>
     </div>
   );
